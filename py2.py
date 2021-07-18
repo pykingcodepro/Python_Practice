6604967ac1e0dc9c79b9a498e1a267b631ee3f42
Pyking <74397688+pykingcodepro@users.noreply.github.com>
2021-07-18 11:45:01 +0530

    Update py2.py

--- py2.py
+++ py2.py
@@ -1,30 +1,30 @@
 # Varibles
 # Integer 
-# a = 10
-# print(type(a))
+a = 10
+print(type(a))
 
 # Decimal / Float
-# a = 10.2
-# print(a)
-# print(type(a))
+a = 10.2
+print(a)
+print(type(a))
 
 # String
-# name = "Vatsal"
-# name = 'Vatsal'
-# print(name)
-# print(type(name))
+name = "Vatsal"
+name = 'Vatsal'
+print(name)
+print(type(name))
 
 # Boolean
-# isComplete = False
-# print(isComplete)
-# print(type(isComplete))
+isComplete = False
+print(isComplete)
+print(type(isComplete))
 
 # Taking value of another variable
-# a = 10
-# b = a
-# print(b)
+a = 10
+b = a
+print(b)
 
 # Multiple Variables at a time
-# a, b = 10, 20
-# print(a)
-# print(b)
+a, b = 10, 20
+print(a)
+print(b)
