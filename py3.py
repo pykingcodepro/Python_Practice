c8aca1f2a05c3c1f07787c739995ed6f9524a2ff
Pyking <74397688+pykingcodepro@users.noreply.github.com>
2021-07-18 20:28:12 +0530

    Create py3.py

---
+++ py3.py
@@ -0,0 +1,18 @@
+# Operators
+## Arithmetic Operators
+'''
+    + -> to add 
+    - -> to subtract 
+    * -> to multiply 
+    / -> to divide 
+    % -> to take remainder
+    // -> for float division
+    ** -> for exponention
+'''
+
+print(10 + 20) # 10+20 = 30
+print(20 - 10) # 20-10 = 10
+print(20 * 10) # 20*10 = 200
+print(20 / 10) # 20/10 = 2.0
+print(20 // 10) # 20//10 = 2
+print(2 ** 5) # 2**5 = 32
