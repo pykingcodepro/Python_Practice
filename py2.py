be7b637f1d0c5e1ed680b3109869cc483ecddcfe
Pyking <74397688+pykingcodepro@users.noreply.github.com>
2021-07-18 11:40:29 +0530

    Create py2.py

---
+++ py2.py
@@ -0,0 +1,30 @@
+# Varibles
+# Integer 
+# a = 10
+# print(type(a))
+
+# Decimal / Float
+# a = 10.2
+# print(a)
+# print(type(a))
+
+# String
+# name = "Vatsal"
+# name = 'Vatsal'
+# print(name)
+# print(type(name))
+
+# Boolean
+# isComplete = False
+# print(isComplete)
+# print(type(isComplete))
+
+# Taking value of another variable
+# a = 10
+# b = a
+# print(b)
+
+# Multiple Variables at a time
+# a, b = 10, 20
+# print(a)
+# print(b)
